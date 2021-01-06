a7d25415aaf1d8e54cc369bc531094b3977d44d8
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-01-06 13:56:15 +0300

    1

--- src/Task1/Triangle.java
+++ src/Task1/Triangle.java
@@ -1,19 +1,19 @@
-package Triangle;
+package Task1;
 
 public class Triangle {
     public static void main(String[] args) {
-        int x = 500;
-        int y = 90;
+        int x = 1;
+        int y = 500;
         int i = 180 - x - y;
 
-        if (i > 0 && i < 180) {   //
+        if (i > 0) {
             System.out.println("такой треугольник есть ");
-        }else {
+
+            if (x == 90 || y == 90 || i == 90) {
+                System.out.println("он прямоугольный");
+            }
+        } else {
             System.out.println("такого треугольника нет");
-        }
-        while (x==90 || y==90|| i == 90){
-            System.out.println("он прямоугольный");
-            break;
         }
     }
 }
---
+++ src/Task2/Number.java
@@ -0,0 +1,7 @@
+package Task2;
+
+public class Number {
+    public static void main(String[] args) {
+        
+    }
+}
