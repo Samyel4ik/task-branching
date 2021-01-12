6e0094ff4db03c67ad42e59897edc30cd998e3b9
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-01-12 15:01:55 +0300

    end

--- src/Task4/Brick.java
+++ src/Task4/Brick.java
@@ -10,15 +10,30 @@ public class Brick {
         int z = 59;
 
         boolean side = x <= B && y <= A; // кирпич проходит боком
-        boolean front = z <= B && y <= A; //
-        boolean up = x <= B && z <= A; //
-        boolean suit = side == true && front == true && up == true;
+        boolean front = z <= B && y <= A; // передом
+        boolean up = x <= B && z <= A; //верхом
+        boolean expand = x <= A && y <= B; // 1 доп условие если перевернуть
 
-        if (suit) {
-            System.out.println("Кирпич проходит в отверстие");
+
+        if (side == true) {
+            System.out.println("Кирпич проходит в отверстие боком");
         } else {
-            System.out.println("Кирпич не проходит в отверстие");
+            System.out.println("Кирпич не проходит в отверстие боком");
         }
-
+        if (front == true) {
+            System.out.println("Кирпич проходит в отверстие передом");
+        } else {
+            System.out.println("Кирпич не проходит в отверстие передом");
+        }
+        if (up == true) {
+            System.out.println("Кирпич проходит в отверстие верхом");
+        } else {
+            System.out.println("Кирпич не проходит в отверстие верхом");
+        }
+        if (expand == true) {
+            System.out.println("Кирпич проходит в отверстие боком если его перевернуть");
+        } else {
+            System.out.println("Кирпич не проходит в отверстие боком если его перевернуть");
+        }
     }
 }
