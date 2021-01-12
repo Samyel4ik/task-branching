2095acc7bb16944629b0182b1ae0e10e53e30c35
Samyel4ik <samsoyk.evgen.92@mail.ru>
2021-01-12 15:47:24 +0300

    yes

--- src/Task4/Brick.java
+++ src/Task4/Brick.java
@@ -5,35 +5,47 @@ public class Brick {
         int A = 40;
         int B = 60;
 
-        int x = 59;
+        int x = 120;
         int y = 59;
-        int z = 59;
+        int z = 39;
 
         boolean side = x <= B && y <= A; // кирпич проходит боком
         boolean front = z <= B && y <= A; // передом
         boolean up = x <= B && z <= A; //верхом
         boolean expand = x <= A && y <= B; // 1 доп условие если перевернуть
+        boolean передомПеревернуть = y <= B && z <= A;
+        boolean верхомПеревернуть = x <= A && z <= B;
 
 
-        if (side == true) {
+        if (side) {
             System.out.println("Кирпич проходит в отверстие боком");
         } else {
             System.out.println("Кирпич не проходит в отверстие боком");
         }
-        if (front == true) {
+        if (front) {
             System.out.println("Кирпич проходит в отверстие передом");
         } else {
             System.out.println("Кирпич не проходит в отверстие передом");
         }
-        if (up == true) {
+        if (up) {
             System.out.println("Кирпич проходит в отверстие верхом");
         } else {
             System.out.println("Кирпич не проходит в отверстие верхом");
         }
-        if (expand == true) {
+        if (expand) {
             System.out.println("Кирпич проходит в отверстие боком если его перевернуть");
         } else {
             System.out.println("Кирпич не проходит в отверстие боком если его перевернуть");
+        }
+        if (передомПеревернуть) {
+            System.out.println("Кирпич проходит в отверстие передом если его перевернуть");
+        } else {
+            System.out.println("Кирпич не проходит в отверстие передом если его перевернуть");
+        }
+        if (верхомПеревернуть) {
+            System.out.println("Кирпич проходит в отверстие верхом если его перевернуть");
+        } else {
+            System.out.println("Кирпич не проходит в отверстие верхом если его перевернуть");
         }
     }
 }
